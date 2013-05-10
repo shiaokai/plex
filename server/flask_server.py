63104d2ebbc3b821d728f974b21ec2e5dc0ae6f2
Kai Wang <kaw006@cs.ucsd.edu>
2013-05-10 16:12:36 -0700

    added basic svt_viewer demo

--- server/flask_server.py
+++ server/flask_server.py
@@ -1,61 +1,76 @@
 import os
 import pdb
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify, make_response, render_template
 from werkzeug import secure_filename
 from flask import send_from_directory
 import urllib
-        
+
+from pano import PanoMap        
+
 import sys
 sys.path.append('../python')
+
 import cPickle
 import settings
 from web_wordspot import WebWordspot
 
+sys.path.append(settings.libsvm_path)
+import svmutil as svm
 
 with open(settings.char_clf_name,'rb') as fid:
     rf = cPickle.load(fid)
 print 'Pre-loaded character classifier'
 
+with open(settings.word_clf_meta_name,'rb') as fid:
+    min_max = cPickle.load(fid)
+svm_clf = svm.svm_load_model(settings.word_clf_name)
+svm_model=(svm_clf,min_max)
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/var/www/plex'
+app.config['CTRL_F_UPLOAD_FOLDER'] = '/var/www/plex'
+app.config['SVT_UPLOAD_FOLDER'] = '/var/www/svt'
 app.debug = False
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+# -----------------------
+# -- STREETVIEW VIEWER --
+# -----------------------
+@app.route('/svt_viewer', methods=['GET'])
+def hello_svt():
+    return render_template('svt_viewer.html')
+
+@app.route('/svt_listener', methods=['GET', 'POST'])
+def svt_listener_url():
+
+    zoom_to_hfov = {'1':90, '2':50, '3':35}
+
+    pano_id = request.form['pano']
+    car_yaw = float(request.form['car-yaw'])
+    img_width = int(request.form['width'])
+    img_height = int(request.form['height'])
+    pitch = float(request.form['pitch'])
+    yaw = float(request.form['yaw'])
+    print request.form['zoom']
+    hfov = zoom_to_hfov[request.form['zoom']] # TODO, map this from 'zoom'
+
+    pano_map = PanoMap(pano_id, car_yaw, app.config['SVT_UPLOAD_FOLDER'])
+    img_cutout_filename = pano_map.cutout(img_width, img_height, pitch, yaw, hfov, override=1)
+    img_result_filename = img_cutout_filename + '_result.jpg'
+    # call SWT+TESS for now
+    
+    call_swt_tess(img_cutout_filename, img_result_filename)
+    
+    r1 = {'result_url':url_for('svt_viewer_file',
+                               filename=os.path.basename(img_result_filename))}
+    return jsonify(r1)
+
+
+# ----------------------
+# -- CTRL-F INTERFACE --
+# ----------------------
 @app.route('/ctrl_f', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        string_to_find = request.form['string_to_find']
-        num_instances = int(request.form['num_instances'])
-        if file:
-            filename = secure_filename(file.filename)
-            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(full_filename)
-            result_filename = filename+'_result.png'
-            full_result_filename = os.path.join(app.config['UPLOAD_FOLDER'],
-                                                result_filename)
-            # call wordspot
-            WebWordspot(full_filename, [string_to_find], num_instances,
-                        full_result_filename, rf)
-            return redirect(url_for('uploaded_file',
-                                    filename=result_filename))
-    return '''
-    <!doctype html>
-    <title>Wordspotting demo</title>
-    <h1>CTRL-F for images</h1>
-    <form action="" method=post enctype=multipart/form-data>
-    <p>Search string: <input type=text name="string_to_find">
-        # instances to find: <input type=text name="num_instances">
-    <p><input type=file name=file>
-    <input type=submit value="Run CTRL-F">
-    </form>
-    '''
-
-
-@app.route('/ctrl_f2', methods=['GET', 'POST'])
 def upload_url():
 
     if request.method == 'POST':
@@ -63,15 +78,15 @@ def upload_url():
         string_to_find = request.form['string_to_find']
         num_instances = int(request.form['num_instances'])
         filename = os.path.basename(img_url)
-        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        full_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'], filename)
 
         urllib.urlretrieve(img_url, full_filename)
 
         result_filename = filename+'_result.png'
-        full_result_filename = os.path.join(app.config['UPLOAD_FOLDER'],
+        full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
                                             result_filename)
         WebWordspot(full_filename, [string_to_find], num_instances,
-                    full_result_filename, rf)
+                    full_result_filename, rf, svm_model=svm_model)
         return redirect(url_for('uploaded_file',
                                 filename=result_filename))
 
@@ -87,25 +102,29 @@ def upload_url():
     </form>
     '''
 
+def call_swt_tess(full_filename, full_result_filename):
+    # call swt
+    swt_dir = settings.swt_dir
+    swt_app = os.path.join(swt_dir, 'swtdetect')
+    swtdraw_app = "tesseract_process.py"
+    cmd = "%s %s | python %s %s %s" % (swt_app, full_filename,
+                                       swtdraw_app, full_filename,
+                                       full_result_filename)
+    os.system(cmd)    
+
 @app.route('/libccv_swt', methods=['GET', 'POST'])
 def upload_swt():
     if request.method == 'POST':
         img_url= request.form['img_url']
         filename = os.path.basename(img_url)
-        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        full_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'], filename)
         urllib.urlretrieve(img_url, full_filename)
         result_filename = filename+'_swt.png'
-        full_result_filename = os.path.join(app.config['UPLOAD_FOLDER'],
+        full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
                                             result_filename)
 
-        # call swt
-        swt_dir = settings.swt_dir
-        swt_app = os.path.join(swt_dir, 'swtdetect')
-        swtdraw_app = "tesseract_process.py"
-        cmd = "%s %s | python %s %s %s" % (swt_app, full_filename,
-                                           swtdraw_app, full_filename,
-                                           full_result_filename)
-        os.system(cmd)
+        call_swt_tess(full_filename, full_result_filename)
+
         return redirect(url_for('uploaded_file',
                                 filename=result_filename))
 
@@ -121,8 +140,14 @@ def upload_swt():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
+    return send_from_directory(app.config['CTRL_F_UPLOAD_FOLDER'],
                                filename)
+
+@app.route('/svt/<filename>')
+def svt_viewer_file(filename):
+    return send_from_directory(app.config['SVT_UPLOAD_FOLDER'],
+                               filename)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ server/pano.py
@@ -0,0 +1,214 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+import random
+from PIL import Image
+from StringIO import StringIO
+import urllib
+import math
+import os
+from numpy import *
+import scipy.linalg
+import scipy
+import math
+import time
+#import pdb
+import cv2
+#from python.maps.tileSystem import *
+import sys
+import pdb
+from matplotlib import pyplot as plt
+
+# Note: code stolen from Tsung-Yi Lin!
+
+def skew(v):
+    if len(v) == 4: v = v[:3]/v[3]
+    skv = roll(roll(diag(v.flatten()), 1, 1), -1, 0)
+    return skv - skv.T
+
+def downloadpano(panoid, fname):
+    gzoom_def = 4;
+    server = math.floor(random.random()*3+1);
+    gzoom = gzoom_def;
+    imtile = fetch_im(gzoom,0,0,panoid,server);
+
+    tilew = 512; tileh = 512;
+    wid = 13;
+    hei = 7;
+    #im = uint8(zeros(length(tiley)*tileh,length(tilex)*tilew,3));
+    I = Image.new('RGB', (tilew*wid, tileh*hei));
+    for i in range(0, wid):
+        for j in range(0, hei):
+            imtile = fetch_im(gzoom,i,j,panoid,server);
+            I.paste(imtile, (tilew*i, tileh*j));
+    I = I.crop([0,0,6656,3328]);
+    print 'save: ' + fname;
+    #cv2.imwrite(fname, asarray(I))
+    I.save(fname);
+    return I
+
+def fetch_im(gzoom,i,j,panoid,server):
+    fn = 'cbk?output=tile&zoom=%d&x=%d&y=%d&cb_client=maps_sv&fover=2&onerr=3&renderer=spherical&v=4&panoid=%s'%(gzoom,i,j,panoid);
+    url = 'https://cbks%d.google.com/%s'%(server,fn);
+    im_file = StringIO(urllib.urlopen(url).read());
+    img = Image.open(im_file);
+    return img;
+
+def iminterpnn(iim, U, V):
+    U = floor(U).astype(int);
+    V = floor(V).astype(int);
+    idx = logical_and(logical_and(0<V, V<iim.shape[0]),  logical_and(0<U, U<iim.shape[1]));
+    oimR=zeros( (U.shape[0], U.shape[1],1))
+    oimG=zeros( (U.shape[0], U.shape[1],1))
+    oimB=zeros( (U.shape[0], U.shape[1],1))
+    oimR[idx, 0] = iim[V[idx], U[idx], 0];
+    oimG[idx, 0] = iim[V[idx], U[idx], 1];
+    oimB[idx, 0] = iim[V[idx], U[idx], 2];
+    oim = concatenate( (oimR, oimG, oimB), 2);
+    return oim;
+
+def Rotate3D_xaxis(x, y, z, pitch):
+    theta = float(pitch) / 180 * math.pi;
+    xx = x;
+    yy = y*math.cos(theta) - z*math.sin(theta);
+    zz = y*math.sin(theta) + z*math.cos(theta);
+    x = xx / abs(zz);
+    y = yy / abs(zz);
+    z = zz / abs(zz);
+    return x, y, z;
+
+def Rotate3D_yaxis(x, y, z, yaw):
+    theta = float(yaw) / 180 * math.pi;
+    yy = y;
+    xx = x*math.cos(theta) + z*math.sin(theta);
+    zz = -x*math.sin(theta) + z*math.cos(theta);
+    x = xx / abs(zz);
+    y = yy / abs(zz);
+    z = zz / abs(zz);
+    return x,y,z;
+
+#def __init__(self, panoid, panopk, heading, lat, lng, folder):
+class PanoMap:
+    def __init__(self, panoid, heading, output_base):
+        # TODO here self._pano is a Django model, change it
+        #self._pano = Pano.objects.get(id = panoid)
+        # in Google API it is pano.getPano()
+        self._panoid = panoid
+        # in Google API it is map.getPhotographerPov().heading
+        self._heading = heading
+        # TODO set the folder to put pano image here
+        self._folder = output_base;
+        self.loadpano()
+        self.somethingcool()
+
+    def somethingcool(self):
+        self._dafuq = 7
+
+    def loadpano(self):
+        pano_folder = self._folder
+        fname = os.path.join(pano_folder,self._panoid + '.jpg');
+
+        if not os.path.exists(pano_folder):
+            os.makedirs(pano_folder)
+
+        if (os.path.exists(fname) ):
+            self._im = asarray(Image.open(fname));
+            #self._im = cv2.imread(fname)
+        else:
+            I = downloadpano(self._panoid, fname);
+            self._im = asarray(I);
+
+    @staticmethod
+    def pixelXYToAngle(x, y, oimw, oimh, pitch, yaw, f):
+        if yaw < -180:
+            yaw = yaw+360
+        if yaw > 180:
+            yaw = yaw-360
+
+        uu = x - float(oimw)/2;
+        vv = y - float(oimh)/2;
+
+        xx,yy,zz = Rotate3D_xaxis(uu, vv, f, pitch);
+        uu,vv,ff = Rotate3D_yaxis(xx, yy, zz, yaw);
+
+        theta = math.atan2(uu, ff);
+        #theta = math.atan(uu/ ff);
+        phi = math.atan(vv / math.sqrt(math.pow(uu,2)+math.pow(ff, 2)) );
+        heading = theta*180/math.pi;
+        pitch = -phi*180/math.pi;
+        return heading, pitch
+
+    @staticmethod
+    def angleToPixelXY(theta, phi,oimw, oimh, pitch, yaw, f):
+        theta = theta *math.pi/180
+        phi = -phi *math.pi/180
+        u = f * math.tan(theta);
+        v = f /(math.cos(theta)) * math.tan(phi);
+        x,y,z = Rotate3D_yaxis(u, v, f, -yaw);
+        x,y,z = Rotate3D_xaxis(x, y, z, -pitch);
+        u = x * z * f
+        v = y * z * f
+
+        x = u + oimw/2;
+        y = v + oimh/2;
+        return array([x, y])
+
+    def cutout(self, oimw, oimh, pitch, yaw, hfov, override): # pitch and yaw are respect to north
+        fpath = self._folder;
+        #if not os.path.exists(fpath):
+        #    os.makedirs(fpath)
+        #fname = '%s/%03d_%f_%d_%d_%d.jpg'%(fpath, yaw, pitch, oimw, oimh, hfov)
+        #fname = os.path.join(fpath, '%03d_%d_%d_%d.jpg' %(yaw, oimw, oimh, hfov))
+        fname = os.path.join(fpath, '%s_%03d_%d_%d_%d.jpg' %(self._panoid, yaw, oimw, oimh, hfov))
+        print fname
+        # =======================  load presave image =========================
+        if os.path.exists(fname) and override != 1:
+            print 'open image from hard drive...'
+            im = Image.open(fname);
+            #im = cv2.imread(fname)
+            return asarray(im);
+        #self.loadpano()
+        iim = self._im
+        iimh=self._im.shape[0];  iimw=self._im.shape[1];      # input  image size
+		
+        pitch = (float(pitch)) / 180 * math.pi;
+        yaw=(float(yaw) - float(self._heading))/180*math.pi;
+        hfov=(float(hfov)/180)*math.pi;
+        f=oimw/(2*math.tan(hfov/2));     # focal length [pix]
+        ouc=(float(oimw+1))/2; ovc=(float(oimh+1))/2;             # output image center
+        iuc=(float(iimw+1))/2; ivc=(float(iimh+1))/2;             # input image center   
+        # Tangent plane to unit sphere mapping
+        X, Y = meshgrid(arange(0,oimw), arange(0,oimh));
+        X = reshape(X-ouc, (1, oimh*oimw) );   Y = reshape(Y-ovc, (1, oimh*oimw) );             # shift origin to the image center
+        Z = f+0*X;  # focal length is defined as z
+        PTS = concatenate((X, Y, Z), 0);
+        # Transformation for oitch angle 0
+        #Tx = SciPy.linalg.expm([0,0,0],[0,0,pitch/180*math.pi],[0, -pitch/180*math.pi, 0]);
+        Tx = scipy.linalg.expm(( [0,0,0],[0,0,pitch],[0, -pitch, 0]));
+
+        PTSt = dot(Tx, PTS);
+        Xt=reshape(PTSt[0,:], (oimh, oimw));
+        Yt=reshape(PTSt[1,:], (oimh, oimw));
+        Zt=reshape(PTSt[2,:], (oimh, oimw));
+    
+        THETA = arctan2(Xt, Zt);                 # cartesian to spherical
+        PHI = arctan(divide(Yt, sqrt(Xt**2+Zt**2))) ;
+        ## Generating cutouts
+        # Image shifting w.r.t. yaw and mapping from unit sphere grid to cylinder
+        sw=iimw/2/math.pi;
+        sh=iimh/math.pi;
+        THETA = THETA+yaw;  # yaw is used here
+        THETA[THETA<math.pi] = THETA[THETA<math.pi] + 2*math.pi;
+        THETA[THETA>=math.pi] = THETA[THETA>=math.pi] - 2*math.pi;
+        
+        U=sw*THETA+iuc; 
+        V=sh*PHI  +ivc;
+        oim=iminterpnn(iim,U,V)
+        #cv2.imwrite(fname, oim)
+        scipy.misc.imsave(fname,oim)
+        #oim = cv2.imread(fname)
+        #return uint8(oim)
+        return fname
+        
