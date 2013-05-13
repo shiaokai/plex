5d687ecc1414f6985b1e6e9af669d80f1f92fbb4
Kai Wang <kaw006@cs.ucsd.edu>
2013-05-12 18:22:29 -0700

    added function for measuring character detection performance

--- python/train_and_bootstrap_char_det.py
+++ python/train_and_bootstrap_char_det.py
@@ -14,14 +14,17 @@ from hog_utils import draw_hog, ReshapeHog
 from char_det import CharDetector, CharDetectorBatch
 
 from time import time
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 
-from sklearn.datasets import fetch_mldata
 from numpy import arange
 import shutil
 
 from display import OutputCharBbs, DrawCharBbs
 from helpers import ReadAllImages
+from evaluation import EvaluateCharacterDetection
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 def TestCharDetector(img, hog, rf, canon_size, alphabet, detect_idxs, save_imgs=False):
     show_times = True
@@ -37,7 +40,7 @@ def ImgsToFeats(I, hog, canon_size):
     feats=np.zeros(0)
 
     for i in range(I.shape[3]):
-        img=cv2.resize(I[:,:,:,i],canon_size)
+        img=cv2.resize(I[:,:,:,i],canon_size,interpolation=cv.CV_INTER_CUBIC)
         feature_vector = hog.compute(img, winStride=(16,16), padding=(0,0))
         if feats.shape[0]==0:
             feats=np.zeros((feature_vector.shape[0],I.shape[3]))
@@ -71,6 +74,7 @@ def TrainCharClassifier(alphabet, train_dir, bg_dir, hog, canon_size,
         t1 = time()
         # NOTE: n_estimators=100 and 'entropy' gives 60% accuracy
         rf = RandomForestClassifier(n_estimators=20, n_jobs=1)
+        #rf = ExtraTreesClassifier(n_estimators=100, max_depth=8,n_jobs=1, criterion='entropy')
         rf.fit(X_train, y_train)
         tTrain = time()
         time_train=tTrain-t1
@@ -118,7 +122,7 @@ def Bootstrap(bs_img_dir, bs_out_dir, bg_dir, canon_size,
     # call batch chardetector
     CharDetectorBatch(bs_img_dir, temp_dir, rf, canon_size, alphabet,
                       detect_idxs=detect_idxs, min_height=min_height,
-                      score_thr=score_thr, num_procs=6)
+                      score_thr=score_thr, num_procs=num_procs)
 
     files = os.listdir(bs_out_dir)
     files.sort()
@@ -151,25 +155,81 @@ def Bootstrap(bs_img_dir, bs_out_dir, bg_dir, canon_size,
             # save
             new_fname = "I%05i.png" % (start_offset + counter)
             full_fname = os.path.join(bs_out_dir, new_fname)
-            patch_100x100=cv2.resize(patch,(100,100))
+            patch_100x100=cv2.resize(patch,(100,100),interpolation=cv.CV_INTER_CUBIC)
             res = cv2.imwrite(full_fname, patch_100x100)
             counter += 1
 
     # clean up
     shutil.rmtree(temp_dir)
 
+def BenchmarkCharacterDetection(rf=None):
+    """
+    Run character detection
+    Produce graph of P/R for each character, and print avg P/R
+    """
+
+    img_dir = settings.img_test_dir
+    gt_dir = settings.img_char_test_gt_dir
+
+    temp_char_dir = 'cache/chars'
+
+    if rf is not None:
+        # batch character detection
+        CharDetectorBatch(img_dir, temp_char_dir, rf, settings.canon_size,
+                          settings.alphabet_master, detect_idxs=settings.detect_idxs,
+                          min_height=settings.min_height,
+                          min_pixel_height=settings.min_pixel_height,
+                          score_thr=settings.score_thr, num_procs=settings.n_procs,
+                          case_mapping=settings.case_mapping,
+                          overlap_thr=settings.overlap_thr)
+    else:
+        print 'No classifier given, just producing results from an old run.'
+
+    eval_results = EvaluateCharacterDetection(gt_dir, temp_char_dir)
+
+    gt_results = eval_results[0]
+    dt_results = eval_results[1]
+    precision = eval_results[2]
+    recall = eval_results[3]
+    thrs = eval_results[4]
+
+    # plot PR
+    n_scatter=50
+    fig = plt.figure()
+    recall_range = np.max(recall) - np.min(recall)
+    recall_step = recall_range / n_scatter
+
+    recall_sc = np.array([recall[0]])
+    precision_sc = np.array([precision[0]])
+    thrs_sc = np.array([thrs[0]])
+    last_recall = recall_sc[-1]
+    for i in range(precision.shape[0]):
+        if recall[i] > (last_recall + recall_step):
+            recall_sc = np.append(recall_sc,recall[i])
+            precision_sc = np.append(precision_sc,precision[i])
+            thrs_sc = np.append(thrs_sc,thrs[i])
+            last_recall = recall[i]
+        
+    plt.clf()
+    plt.scatter(recall_sc, precision_sc, c=thrs_sc,marker=',',edgecolor='none')
+    plt.colorbar()
+    plt.hold(True)
+    plt.plot(recall, precision,'g')
+    plt.axis((0,1,0,1))
+    plt.savefig(os.path.join(settings.fig_dir,"pr_char_curves.pdf"))
+
+    
 def main():
-    '''
     # 1. train a base classifier
-    rf=TrainCharClassifier(settings.alphabet_master,
-                           settings.char_train_dir,
-                           settings.char_train_bg_dir,
-                           settings.hog,
-                           settings.canon_size,
-                           clf_path=settings.initial_char_clf_name,
-                           max_per_class=settings.max_per_class,
-                           max_bg=settings.max_bg,
-                           force=False)
+    rf = TrainCharClassifier(settings.alphabet_master,
+                             settings.char_train_dir,
+                             settings.char_train_bg_dir,
+                             settings.hog,
+                             settings.canon_size,
+                             clf_path=settings.initial_char_clf_name,
+                             max_per_class=settings.max_per_class,
+                             max_bg=settings.max_bg,
+                             force=True)
 
     # 2. 62-way
     TestCharClassifier(settings.alphabet_detect,
@@ -188,18 +248,25 @@ def main():
               settings.detect_idxs,
               rf,
               num_procs=settings.n_procs)
-    '''
+
     # 4. train a classifier after bootstrap
-    TrainCharClassifier(settings.alphabet_master,
-                        settings.char_train_dir,
-                        settings.bootstrap_bg_dir,
-                        settings.hog,
-                        settings.canon_size,
-                        clf_path=settings.char_clf_name,
-                        max_per_class=settings.max_per_class,
-                        max_bg=settings.max_bg,
-                        force=True)
+    rf = TrainCharClassifier(settings.alphabet_master,
+                             settings.char_train_dir,
+                             settings.bootstrap_bg_dir,
+                             settings.hog,
+                             settings.canon_size,
+                             clf_path=settings.char_clf_name,
+                             max_per_class=settings.max_per_class,
+                             max_bg=settings.max_bg_after_bs,
+                             force=True)
+
+    # 5. evaluate character detection
+    #BenchmarkCharacterDetection(rf);
+    """
+    BenchmarkCharacterDetection();
+    """
 
 if __name__=="__main__":
-    cProfile.run('main()','profile_detection')
+    #cProfile.run('main()','profile_detection')
+    main()
     
