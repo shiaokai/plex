26eec7e621ce4e8c207ab25e551a99f34418d330
Kai Wang <kaw006@cs.ucsd.edu>
2013-05-13 16:22:20 -0700

    operational, but crappy, ctrl-f and street view demo

--- server/flask_server.py
+++ server/flask_server.py
@@ -31,28 +31,13 @@ with open(settings.word_clf_meta_name,'rb') as fid:
     alpha_min_max = cPickle.load(fid)
 alpha = alpha_min_max[0]
 min_max = (alpha_min_max[1], alpha_min_max[2])
-svm_clf = svm.svm_load_model(settings.word_clf_name)
+svm_clf = svm.svm_load_model(settings.word_clf_poly_name)
 svm_model=(svm_clf,min_max)
 
 app = Flask(__name__)
 app.config['CTRL_F_UPLOAD_FOLDER'] = '/var/www/plex'
 app.config['SVT_UPLOAD_FOLDER'] = '/var/www/svt'
 app.debug = False
-
-
-"""
-Copied from stack overflow:
-http://stackoverflow.com/questions/13768007/browser-caching-issues-in-flask
-"""
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
 
 @app.route('/')
 def hello_world():
@@ -69,11 +54,22 @@ def hello_svt():
 def show_result():
     orig_image = request.args['orig_image']
     result_image = request.args['result_image']
-    #return_link = request.args['return_link'] -- TODO add this
-    return render_template('result.html', orig_image=orig_image, result_image=result_image)
+    return_link = request.args['return_link']
+    if request.args.has_key('char_bbs_image'):
+        char_bbs_image = request.args['char_bbs_image']
+        return render_template('result.html', orig_image=orig_image,
+                               result_image=result_image,
+                               return_link=return_link,
+                               char_bbs_image=char_bbs_image)
+    else:
+        return render_template('result.html', orig_image=orig_image,
+                               result_image=result_image,
+                               return_link=return_link)
+                        
 
-@app.route('/svt_listener', methods=['GET', 'POST'])
-def svt_listener_url():
+
+@app.route('/svt_run_swt_tess', methods=['GET', 'POST'])
+def svt_run_swt_tess():
 
     zoom_to_hfov = {'1':90, '2':45, '3':25, '4':15}
 
@@ -81,10 +77,10 @@ def svt_listener_url():
     car_yaw = float(request.form['car-yaw'])
     img_width = int(request.form['width'])
     img_height = int(request.form['height'])
-    pitch = float(request.form['pitch'])
+    pitch = -1*float(request.form['pitch'])
     yaw = float(request.form['yaw'])
     print request.form['zoom']
-    hfov = zoom_to_hfov[request.form['zoom']] # TODO, map this from 'zoom'
+    hfov = zoom_to_hfov[request.form['zoom']]
 
     pano_map = PanoMap(pano_id, car_yaw, app.config['SVT_UPLOAD_FOLDER'])
     img_cutout_filename = pano_map.cutout(img_width, img_height, pitch, yaw, hfov, override=1)
@@ -92,9 +88,36 @@ def svt_listener_url():
     # call SWT+TESS for now
     
     print 'business search result: ', request.form['business-text']
-    SwtAndTesseract(img_cutout_filename, img_result_filename)
+    RunSwtAndTesseract(img_cutout_filename, img_result_filename)
     
-    params = urllib.urlencode({'orig_image': url_for('svt_viewer_file', filename=os.path.basename(img_cutout_filename)), 'result_image': url_for('svt_viewer_file', filename=os.path.basename(img_result_filename))})
+    params = urllib.urlencode({'orig_image': url_for('svt_viewer_file', filename=os.path.basename(img_cutout_filename)), 'result_image': url_for('svt_viewer_file', filename=os.path.basename(img_result_filename)), 'return_link': 'svt_viewer'})
+    result_page = "/svt_result?%s" % params
+    r1 = {'result_url': result_page}
+    return jsonify(r1)
+
+@app.route('/svt_run_plex', methods=['GET', 'POST'])
+def svt_run_plex():
+
+    zoom_to_hfov = {'1':90, '2':45, '3':25, '4':15}
+
+    pano_id = request.form['pano']
+    car_yaw = float(request.form['car-yaw'])
+    img_width = int(request.form['width'])
+    img_height = int(request.form['height'])
+    pitch = -1*float(request.form['pitch'])
+    yaw = float(request.form['yaw'])
+    print request.form['zoom']
+    hfov = zoom_to_hfov[request.form['zoom']]
+
+    pano_map = PanoMap(pano_id, car_yaw, app.config['SVT_UPLOAD_FOLDER'])
+    img_cutout_filename = pano_map.cutout(img_width, img_height, pitch, yaw, hfov, override=1)
+    img_result_filename = img_cutout_filename + '_result.jpg'
+    
+    lexicon = StringToLexicon(str(request.form['business-text']))
+    RunPlex(img_cutout_filename, lexicon, 1,
+            img_result_filename, rf, alpha, svm_model=svm_model)
+            
+    params = urllib.urlencode({'orig_image': url_for('svt_viewer_file', filename=os.path.basename(img_cutout_filename)), 'result_image': url_for('svt_viewer_file', filename=os.path.basename(img_result_filename)), 'return_link': 'svt_viewer'})
     result_page = "/svt_result?%s" % params
     r1 = {'result_url': result_page}
     return jsonify(r1)
@@ -108,25 +131,48 @@ def upload_url():
 
     if request.method == 'POST':
         img_url= request.form['img_url']
-        string_to_find = request.form['string_to_find']
-        num_instances = int(request.form['num_instances'])
         filename = os.path.basename(img_url)
         full_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'], filename)
-
         urllib.urlretrieve(img_url, full_filename)
 
-        result_filename = filename+'_result.png'
-        full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
-                                            result_filename)
-        WebWordspot(full_filename, [string_to_find], num_instances,
-                    full_result_filename, rf, alpha, svm_model=svm_model)
+        if request.form['algo'] == 'swt_tess':
+            # Call SWT + TESS
+            result_filename = filename+'_swt.png'
+            full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
+                                                result_filename)
+            RunSwtAndTesseract(full_filename, full_result_filename)
 
-        params = urllib.urlencode({'orig_image': url_for('uploaded_file', filename=filename),
-                                   'result_image': url_for('uploaded_file', filename=result_filename),
-                                   'return_link': 'ctrl_f',
-                                   'notes':'try hitting F5 if the result is weird'})
+            params = urllib.urlencode({'orig_image': url_for('uploaded_file', filename=filename),
+                                       'result_image': url_for('uploaded_file', filename=result_filename),
+                                       'return_link': 'ctrl_f'})
+        else:
+            string_to_find = request.form['string_to_find']
+            num_instances = int(request.form['num_instances'])
+            # Call PLEX
+            result_filename = filename+'_plex.png'
+            full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
+                                                result_filename)
+            # character debug information
+            full_char_bbs_filename = None
+            if request.form.has_key('show_debug'):
+                char_bbs_filename = result_filename + '_bbs.png'
+                full_char_bbs_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
+                                                      char_bbs_filename)
+                params = urllib.urlencode({'orig_image': url_for('uploaded_file', filename=filename),
+                                           'result_image': url_for('uploaded_file', filename=result_filename),
+                                           'return_link': 'ctrl_f',
+                                           'char_bbs_image': url_for('uploaded_file', filename=char_bbs_filename)})
+            else:
+                params = urllib.urlencode({'orig_image': url_for('uploaded_file', filename=filename),
+                                           'result_image': url_for('uploaded_file', filename=result_filename),
+                                           'return_link': 'ctrl_f'})
+            
+            RunPlex(full_filename, [string_to_find], num_instances,
+                    full_result_filename, rf, alpha, svm_model=svm_model,
+                    show_char_bbs=full_char_bbs_filename)
+
+
         result_page = "/svt_result?%s" % params
-
         return redirect(result_page)
 
     return '''
@@ -136,35 +182,11 @@ def upload_url():
     <form action="" method=post>
     <p>Search string: <input type=text name="string_to_find">
     # instances to find: <input type=text name="num_instances">
-    <p><input type=text name="img_url">
+    <input type="checkbox" name="show_debug" value="yes">Show debug information.<br>
+    <p>Image URL:<input type=text name="img_url">
     <input type=submit value="Run CTRL-F">
-    </form>
-    '''
-
-
-@app.route('/libccv_swt', methods=['GET', 'POST'])
-def upload_swt():
-    if request.method == 'POST':
-        img_url= request.form['img_url']
-        filename = os.path.basename(img_url)
-        full_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'], filename)
-        urllib.urlretrieve(img_url, full_filename)
-        result_filename = filename+'_swt.png'
-        full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
-                                            result_filename)
-
-        SwtAndTesseract(full_filename, full_result_filename)
-
-        return redirect(url_for('uploaded_file',
-                                filename=result_filename))
-
-    return '''
-    <!doctype html>
-    <title>libccv-SWT demo</title>
-    <h1>Display output of libccv-swt text detector</h1>
-    <form action="" method=post>
-    <p><input type=text name="img_url">
-    <input type=submit value="Run SWT">
+    <input type="radio" name="algo" value="swt_tess" checked>SWT+TESS</input>
+    <input type="radio" name="algo" value="plex">PLEX</input>
     </form>
     '''
 
@@ -181,7 +203,19 @@ def svt_viewer_file(filename):
 # ----------------------
 # HELPER FUNCTIONS
 # ----------------------
-def SwtAndTesseract(full_filename, full_result_filename):
+def StringToLexicon(input_string):
+    # requirements: business string must have at least 3 characters
+    #               and can only be alphanum
+    lexicon = []
+    names = input_string.split(' ')
+    for name in names:
+        filtered_name = "".join(letter for letter in name if letter.isalnum())
+        if len(filtered_name) > 2:
+            lexicon.append(filtered_name)
+
+    return lexicon
+
+def RunSwtAndTesseract(full_filename, full_result_filename):
     swt_dir = settings.swt_dir
     swt_app = os.path.join(swt_dir, 'swtdetect')
     swtdraw_app = "tesseract_process.py"
@@ -190,8 +224,8 @@ def SwtAndTesseract(full_filename, full_result_filename):
                                        full_result_filename)
     os.system(cmd)    
 
-def WebWordspot(img_name, lexicon, max_locations, result_path, rf, alpha, svm_model=None,
-                debug_img_name=None):
+def RunPlex(img_name, lexicon, max_locations, result_path, rf, alpha, svm_model=None,
+            debug_img_name=None, show_char_bbs=None):
     img = cv2.imread(img_name)    
     (match_bbs, char_bbs) = WordSpot(img, lexicon, alpha, settings, use_cache=False,
                                      img_name=img_name, max_locations=max_locations,
@@ -200,10 +234,9 @@ def WebWordspot(img_name, lexicon, max_locations, result_path, rf, alpha, svm_mo
     DrawWordResults(img, match_bbs)
     plt.savefig(result_path)
 
-    DebugCharBbs(img, char_bbs, settings.alphabet_master, lexicon)
-    debug_path = result_path+'_dbg.png' 
-    plt.savefig(debug_path)
-    return debug_path
+    if show_char_bbs is not None:
+        DebugCharBbs(img, char_bbs, settings.alphabet_master, lexicon)
+        plt.savefig(show_char_bbs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
