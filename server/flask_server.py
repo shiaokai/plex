a29c2261a9faf191c9714112d2b060ce0385a8c7
Kai Wang <kaw006@cs.ucsd.edu>
2013-05-12 20:42:22 -0700

    added local settings for flask server config

--- server/flask_server.py
+++ server/flask_server.py
@@ -7,24 +7,30 @@ import urllib
 
 from pano import PanoMap        
 
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import cv2
+
 import sys
-sys.path.append('../python')
 
 import cPickle
-import settings
-from web_wordspot import WebWordspot
-
+import settings # grab server directory's settings, not python directory
 sys.path.append(settings.libsvm_path)
 import svmutil as svm
 
-import urllib
+sys.path.append(os.path.join('..','python'))
+from display import DrawWordResults, DebugCharBbs
+from wordspot import WordSpot
 
 with open(settings.char_clf_name,'rb') as fid:
     rf = cPickle.load(fid)
 print 'Pre-loaded character classifier'
 
 with open(settings.word_clf_meta_name,'rb') as fid:
-    min_max = cPickle.load(fid)
+    alpha_min_max = cPickle.load(fid)
+alpha = alpha_min_max[0]
+min_max = (alpha_min_max[1], alpha_min_max[2])
 svm_clf = svm.svm_load_model(settings.word_clf_name)
 svm_model=(svm_clf,min_max)
 
@@ -32,6 +38,22 @@ app = Flask(__name__)
 app.config['CTRL_F_UPLOAD_FOLDER'] = '/var/www/plex'
 app.config['SVT_UPLOAD_FOLDER'] = '/var/www/svt'
 app.debug = False
+
+
+"""
+Copied from stack overflow:
+http://stackoverflow.com/questions/13768007/browser-caching-issues-in-flask
+"""
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -47,6 +69,7 @@ def hello_svt():
 def show_result():
     orig_image = request.args['orig_image']
     result_image = request.args['result_image']
+    #return_link = request.args['return_link'] -- TODO add this
     return render_template('result.html', orig_image=orig_image, result_image=result_image)
 
 @app.route('/svt_listener', methods=['GET', 'POST'])
@@ -69,12 +92,10 @@ def svt_listener_url():
     # call SWT+TESS for now
     
     print 'business search result: ', request.form['business-text']
-    call_swt_tess(img_cutout_filename, img_result_filename)
+    SwtAndTesseract(img_cutout_filename, img_result_filename)
     
     params = urllib.urlencode({'orig_image': url_for('svt_viewer_file', filename=os.path.basename(img_cutout_filename)), 'result_image': url_for('svt_viewer_file', filename=os.path.basename(img_result_filename))})
     result_page = "/svt_result?%s" % params
-    #r1 = {'result_url':url_for('svt_viewer_file',
-    #                           filename=os.path.basename(img_result_filename))}
     r1 = {'result_url': result_page}
     return jsonify(r1)
 
@@ -98,9 +119,15 @@ def upload_url():
         full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
                                             result_filename)
         WebWordspot(full_filename, [string_to_find], num_instances,
-                    full_result_filename, rf, svm_model=svm_model)
-        return redirect(url_for('uploaded_file',
-                                filename=result_filename))
+                    full_result_filename, rf, alpha, svm_model=svm_model)
+
+        params = urllib.urlencode({'orig_image': url_for('uploaded_file', filename=filename),
+                                   'result_image': url_for('uploaded_file', filename=result_filename),
+                                   'return_link': 'ctrl_f',
+                                   'notes':'try hitting F5 if the result is weird'})
+        result_page = "/svt_result?%s" % params
+
+        return redirect(result_page)
 
     return '''
     <!doctype html>
@@ -114,15 +141,6 @@ def upload_url():
     </form>
     '''
 
-def call_swt_tess(full_filename, full_result_filename):
-    # call swt
-    swt_dir = settings.swt_dir
-    swt_app = os.path.join(swt_dir, 'swtdetect')
-    swtdraw_app = "tesseract_process.py"
-    cmd = "%s %s | python %s %s %s" % (swt_app, full_filename,
-                                       swtdraw_app, full_filename,
-                                       full_result_filename)
-    os.system(cmd)    
 
 @app.route('/libccv_swt', methods=['GET', 'POST'])
 def upload_swt():
@@ -135,7 +153,7 @@ def upload_swt():
         full_result_filename = os.path.join(app.config['CTRL_F_UPLOAD_FOLDER'],
                                             result_filename)
 
-        call_swt_tess(full_filename, full_result_filename)
+        SwtAndTesseract(full_filename, full_result_filename)
 
         return redirect(url_for('uploaded_file',
                                 filename=result_filename))
@@ -160,6 +178,32 @@ def svt_viewer_file(filename):
     return send_from_directory(app.config['SVT_UPLOAD_FOLDER'],
                                filename)
 
+# ----------------------
+# HELPER FUNCTIONS
+# ----------------------
+def SwtAndTesseract(full_filename, full_result_filename):
+    swt_dir = settings.swt_dir
+    swt_app = os.path.join(swt_dir, 'swtdetect')
+    swtdraw_app = "tesseract_process.py"
+    cmd = "%s %s | python %s %s %s" % (swt_app, full_filename,
+                                       swtdraw_app, full_filename,
+                                       full_result_filename)
+    os.system(cmd)    
+
+def WebWordspot(img_name, lexicon, max_locations, result_path, rf, alpha, svm_model=None,
+                debug_img_name=None):
+    img = cv2.imread(img_name)    
+    (match_bbs, char_bbs) = WordSpot(img, lexicon, alpha, settings, use_cache=False,
+                                     img_name=img_name, max_locations=max_locations,
+                                     svm_model=svm_model, rf_preload=rf)
+
+    DrawWordResults(img, match_bbs)
+    plt.savefig(result_path)
+
+    DebugCharBbs(img, char_bbs, settings.alphabet_master, lexicon)
+    debug_path = result_path+'_dbg.png' 
+    plt.savefig(debug_path)
+    return debug_path
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ server/settings.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import os
+from cfg_synth import *
+#from cfg_train_icdar_test_icdar import *
+
+libsvm_path='/home/shiaokai/projects/third_party/libsvm/libsvm-3.16/python/'
+swt_dir='/home/shiaokai/projects/github/ccv/bin/'
+
+hog = cv2.HOGDescriptor((16,16),(16,16),(16,16),(8,8),9,1,-1)
+canon_size = (48, 48)
+
+alphabet_master='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_'
+alphabet_detect='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+alphabet_upper='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+char_train_bg_dir = '/data/text/plex/msrc/train/charBg'
+char_test_bg_dir = '/data/text/plex/msrc/test/charBg'
+bootstrap_img_dir = '/data/text/plex/msrc/train/images'
+
+project_dir = '/home/shiaokai/projects/github/plex/python'
+stored_dir = 'data_' + base_name
+if not(os.path.isdir(stored_dir)):
+    os.makedirs(stored_dir)
+    
+initial_char_clf_name = os.path.join(stored_dir, 'char_clf_initial.dat')
+char_clf_name = os.path.join(stored_dir, 'char_clf_final.dat')
+word_clf_name = os.path.join(stored_dir, 'word_clf.dat')
+word_clf_meta_name = os.path.join(stored_dir, 'word_clf_meta.dat')
+word_clf_poly_name = os.path.join(stored_dir, 'word_clf_poly.dat')
+
+n_procs = 6
+
+# char params
+overlap_thr=0.5
+score_thr=0.1
+min_height=0.05
+min_pixel_height=10
+
+fig_dir = os.path.join('/home/shiaokai/Dropbox', base_name)
+if not(os.path.isdir(fig_dir)):
+    os.makedirs(fig_dir)
+
+# word params
+max_locations = 5
+
+detect_idxs=[]
+for alpha0 in alphabet_detect:
+    detect_idxs.append(alphabet_master.find(alpha0))
+
+cache_dir = 'cache'
+
+case_mapping = []
+for i in range(len(alphabet_detect)):
+    case_mapping.append(alphabet_upper.find(alphabet_detect[i].upper()))
+    
